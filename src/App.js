e9224ce336f7869e267793531242485004122850
what-it-izz <main4izzy@gmail.com>
2023-06-02 00:03:11 -0700

    added photo & trying to add video

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TwoColumnLayout from './Columns';
 import LavaLampBackground from './LavaLampBackground';
 import './Styles.css';
 // import Header from './Header';
-import { Header, Segment } from 'semantic-ui-react'
+// import { Header, Segment } from 'semantic-ui-react'
 // import './Styles.scss'; // Import your Sass file
 
 function App() {
@@ -12,19 +12,35 @@ function App() {
         <header className='floating-header'>
           <nav class="header-nav">
             <ul class="link-list">
+            <li><a href="#">Home</a></li>
               <li><a href="#">Blog</a></li>
+              <li><a href="#">Github</a></li>
+              <li><a href="#">Linkedin</a></li>
               <li><a href="#">Spotify</a></li>
-              <li><a href="#">Contact Me</a></li>
             </ul>
           </nav>
         </header>
       {/* </div> */}
       <div className="container">
-        <TwoColumnLayout
-          leftContent={<p>This is the left column.</p>}
-          rightContent={<p>This is the right column.</p>}
+        <img
+          src={"./me.jpg"}
+          alt="Photo"
+          style={{
+            width: "200px", // Adjust the desired width
+            height: "200px", // Adjust the desired height
+            borderRadius: "50%", // Make the image round
+            objectFit: "cover", // Maintain aspect ratio and fill the container
+            position:"absolute", 
+            top:"10px",
+            left:"80px",
+          }}
         />
-        <LavaLampBackground />
+        <iframe width="560" height="315" src="https://youtu.be/PjXNHwK15CQ" frameborder="0" allowfullscreen></iframe>
+        {/* <TwoColumnLayout
+            leftContent={<p>This is the left column.</p>}
+            rightContent={<p>This is the right column.</p>}
+        />
+        <LavaLampBackground /> */}
       </div>
     </div>
   );
