7d5ee3b7c9408e7f33efaea027e96ee1e83c3b9c
what-it-izz <main4izzy@gmail.com>
2023-06-09 22:11:17 -0700

    save progress

--- src/App.js
+++ src/App.js
@@ -8,43 +8,43 @@ import './Styles.css';
 function App() {
   return (
     <div>
-        <header className='floating-header'>
-          <nav class="header-nav">
-            <ul class="link-list">
-            <li><a href="#">Home</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Github</a></li>
-              <li><a href="#">Linkedin</a></li>
-              <li><a href="#">Spotify</a></li>
-            </ul>
-          </nav>
-        </header>
+      <header className='floating-header'>
+        <nav class="header-nav">
+          <ul class="link-list">
+          <li><a href="#">Home</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Github</a></li>
+            <li><a href="#">Linkedin</a></li>
+            <li><a href="#">Spotify</a></li>
+          </ul>
+        </nav>
+      </header>
       <div className="container">
+        <div>
         <img
-          src={"./me.jpg"}
-          alt="Photo"
-          style={{
-            width: "200px", // Adjust the desired width
-            height: "200px", // Adjust the desired height
-            borderRadius: "50%", // Make the image round
-            objectFit: "cover", // Maintain aspect ratio and fill the container
-            position:"absolute", 
-            top:"10px",
-            left:"80px",
-          }}
-        />
-        <iframe 
-          width="560" 
-          height="315" 
-          src="https://www.youtube.com/embed/PjXNHwK15CQ" 
-          frameborder="0" 
-          allowfullscreen
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-        </iframe>
-        {/* <TwoColumnLayout
-            leftContent={<p>This is the left column.</p>}
-            rightContent={<p>This is the right column.</p>}
-        /> */}
+            src={"./me.jpg"}
+            alt="Photo"
+            style={{
+              width: "200px", // Adjust the desired width
+              height: "200px", // Adjust the desired height
+              borderRadius: "50%", // Make the image round
+              objectFit: "cover", // Maintain aspect ratio and fill the container
+              position:"absolute", 
+              top:"80px",
+              left:"80px",
+            }}
+          />
+        </div>
+        <div>
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/PjXNHwK15CQ" 
+            frameborder="0" 
+            allowfullscreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+          </iframe>
+        </div>
         <LavaLampBackground />
       </div>
     </div>
